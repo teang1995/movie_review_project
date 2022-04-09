758b94f3237df1fe18c6c262ba0cdbf03b78de68
teang1995 <teang1995@ajou.ac.kr>
2022-04-09 17:40:06 +0900

    [#4] make run_server.sh -> run server without any additional cmd

---
+++ run_server.sh
@@ -0,0 +1,1 @@
+docker run --rm -itd -p 8080:8080 -v $(pwd):/code --name spring_server spring_docker_test:v0.1 /bin/bash -c "cd /code/hello-spring && ./gradlew build && java -jar /code/hello-spring/build/libs/*" 
